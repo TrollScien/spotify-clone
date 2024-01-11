19ecb354ba75ef45c28299740961a05b5fd955d0
Guillermo Thourey <thoureyg@gmail.com>
2024-01-11 01:25:46 -0400

    Refactor player

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,7 +1,8 @@
 import { usePlayerStore } from "@/store/playerStore"
-import { useEffect, useRef, useState } from "react"
-import { Slider } from "./Slider"
+import { useEffect, useRef } from "react"
 import { VolumeControl } from "./VolumeControl"
+import { SongControl } from "./SongControl"
+import { CurrentSong } from "./CurrentSong"
 export const Pause = ({ className }) => (
     <svg className={className} role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>
 )
@@ -17,66 +18,6 @@ export const VolumeSilence = () => (
 export const Volume = () => (
     <svg fill="currentColor" role="presentation" height="16" width="16" aria-hidden="true" aria-label="Volumen alto" id="volume-icon" viewBox="0 0 16 16"><path d="M9.741.85a.75.75 0 0 1 .375.65v13a.75.75 0 0 1-1.125.65l-6.925-4a3.642 3.642 0 0 1-1.33-4.967 3.639 3.639 0 0 1 1.33-1.332l6.925-4a.75.75 0 0 1 .75 0zm-6.924 5.3a2.139 2.139 0 0 0 0 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 4.29V5.56a2.75 2.75 0 0 1 0 4.88z"></path><path d="M11.5 13.614a5.752 5.752 0 0 0 0-11.228v1.55a4.252 4.252 0 0 1 0 8.127v1.55z"></path></svg>
 )
-
-const CurrentSong = ({ image, title, artists }: { image: string, title: string, artists: [string] }) => {
-    return (
-        <div className="flex items-center gap-5 relative overflow-hidden">
-            <picture className="size-16 bg-zinc-800 rounded-md shadow-lg overflow-hidden">
-                <img src={image} alt={title} />
-            </picture>
-            <div className="flex flex-col">
-
-                <h3 className="font-semibold text-sm block">
-                    {title}
-                </h3>
-                <span className="text-xs opacity-80">
-                    {artists?.join(', ')}
-                </span>
-            </div>
-        </div>
-    )
-}
-
-const SongControl = ({ audio }) => {
-    const [currentTime, setCurrentTime] = useState(0)
-
-    useEffect(() => {
-        audio.current.addEventListener('timeupdate', handleTimeUpdate)
-        return () => {
-            audio.current.removeEventListener('timeupdate', handleTimeUpdate)
-        }
-    }, [])
-
-    const handleTimeUpdate = () => {
-        setCurrentTime(audio.current.currentTime)
-    }
-    const formatTime = time => {
-        if (time == null) return `0:00`
-        const seconds = Math.floor(time % 60)
-        const minutes = Math.floor(time / 60)
-
-        return `${minutes}:${seconds.toString().padStart(2, '0')}`
-    }
-    const duration = audio?.current?.duration ?? 0
-
-    return (
-        <div className="flex gap-x-3 text-xs pt-2">
-            <span className="opacity-50 w-12 text-right">{formatTime(currentTime)}</span>
-
-            <Slider
-                value={[currentTime]}
-                max={audio?.current?.duration ?? 0}
-                min={0}
-                className="w-[400px]"
-                onValueChange={(value) => {
-                    const [newCurrentTime] = value
-                    audio.current.currentTime = newCurrentTime
-                }} />
-
-            <span className="opacity-50 w-12">{duration ? formatTime(duration) : null}</span>
-        </div>
-    )
-}
 
 export function Player() {
     const { isPlaying, setIsPlaying, currentMusic, volume } = usePlayerStore(state => state)
